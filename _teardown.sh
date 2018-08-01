bc4d388da99aa2dc510a22ea6bcf82611b98e598
snobu <foo@snobu.org>
2018-08-01 12:00:43 +0300

    Fix cluster teardown

--- _teardown.sh
+++ _teardown.sh
@@ -1,5 +1,5 @@
 echo "[TEARDOWN]\nCurrent Kube context: `kubectl config current-context`\n"
 kubectl delete pod yololens --now
+az aks scale -n 'yololens-cluster' -g 'yololens-cluster' -c 1 -o table
 az vm deallocate -n 'aks-nodepool1-41528601-0' -g 'MC_YOLOLENS-CLUSTER_YOLOLENS-CLUSTER_WESTEUROPE' --no-wait -o table
-az aks scale -n 'yololens-cluster' -g 'yololens-cluster' -c 1 --no-wait -o table
 echo '--- TEARDOWN COMPLETE. ---'
